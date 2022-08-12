a98114288407bbde1e99fa4023c326a979933c57
Luciano <Luchoalexisbarboza@gmail.com>
2022-08-12 05:49:47 -0300

    Agregado eleccion de usuario

--- Proyecto.js
+++ Proyecto.js
@@ -1,9 +1,39 @@
+let random = ["Piedra", "Papel", "Tijera"];
+
+
 function getComputerChoice() {
     return random [Math.floor(Math.random()*random.length)];
 }
+function getPlayerChoice() {
+    let getPlayerChoice = prompt("Piedra, Papel o Tijera?");
+    return getPlayerChoice;
+}
 
-const random = ["Piedra", "Papel", "Tijera"];
-console.log(getComputerChoice());
+const computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice();
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Piedra") {
+        if (computerSelection === "Piedra") {
+            console.log("Empate!");
+        } else if (computerSelection === "Papel") {
+            console.log("Perdiste!");
+        } else console.log("Ganaste!");
+    } else if (playerSelection === "Papel") {
+        if (computerSelection === "Piedra") {
+            console.log("Ganaste!");
+        } else if (computerSelection === "Papel") {
+            console.log("Empate!"); 
+        } else console.log("Perdiste!")
+    } else if (playerSelection === "Tijera") {
+        if (computerSelection === "Piedra") {
+            console.log("Perdiste!");
+        } else if (computerSelection === "Papel") {
+            console.log("Ganaste!"); 
+        } else console.log("Empate!")
+    } else console.log("ERROR: Tenes que introducir una de las 3 opciones!");
+}
+
+console.log(playRound(playerSelection, computerSelection));
 
 
