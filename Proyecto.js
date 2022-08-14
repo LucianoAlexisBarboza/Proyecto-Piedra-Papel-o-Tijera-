f79dd3925b1fd42ecc2124d7c635b7e9bf13e4c7
Luciano <Luchoalexisbarboza@gmail.com>
2022-08-14 01:05:10 -0300

    V1 Finalizada, Traducido al ingles

--- Proyecto.js
+++ Proyecto.js
@@ -1,39 +1,58 @@
-let random = ["Piedra", "Papel", "Tijera"];
-
+let random = ["Rock", "Paper", "Scissors"]
 
 function getComputerChoice() {
     return random [Math.floor(Math.random()*random.length)];
 }
-function getPlayerChoice() {
-    let getPlayerChoice = prompt("Piedra, Papel o Tijera?");
-    return getPlayerChoice;
+
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
+    if (playerSelection === "Rock") {
+        if (computerSelection === "Paper") {
+            computerScore++;
+            console.log("You lose");
+        } else if (computerSelection === "Scissors") {
+            playerScore++;
+            console.log("You win");
+        } else console.log("Draw");
+    } else if (playerSelection === "Paper") {
+        if (computerSelection === "Rock") {
+            playerScore++;
+            console.log("You win");
+        } else if (computerSelection === "Scissors") {
+            computerScore++;
+            console.log("You lose"); 
+        } else console.log("Draw");
+    } else if (playerSelection === "Scissors") {
+        if (computerSelection === "Rock") {
+            computerScore++;
+            console.log("You lose");
+        } else if (computerSelection === "Paper") {
+            playerScore++;
+            console.log("You win"); 
+        } else console.log("Draw");
+    }
 }
 
-const computerSelection = getComputerChoice();
-const playerSelection = getPlayerChoice();
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "Piedra") {
-        if (computerSelection === "Piedra") {
-            console.log("Empate!");
-        } else if (computerSelection === "Papel") {
-            console.log("Perdiste!");
-        } else console.log("Ganaste!");
-    } else if (playerSelection === "Papel") {
-        if (computerSelection === "Piedra") {
-            console.log("Ganaste!");
-        } else if (computerSelection === "Papel") {
-            console.log("Empate!"); 
-        } else console.log("Perdiste!")
-    } else if (playerSelection === "Tijera") {
-        if (computerSelection === "Piedra") {
-            console.log("Perdiste!");
-        } else if (computerSelection === "Papel") {
-            console.log("Ganaste!"); 
-        } else console.log("Empate!")
-    } else console.log("ERROR: Tenes que introducir una de las 3 opciones!");
+let computerScore = 0;
+let playerScore = 0;
+
+function game(totalScore) {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Rock, Paper or Scissors?");
+        playRound(playerSelection)
+     }
 }
 
-console.log(playRound(playerSelection, computerSelection));
+function score() {
+    if (computerScore > playerScore) {
+        console.log("You lose this game, press F5 to start again.")
+    } else if (playerScore > computerScore) {
+        console.log("You win this game, Congratulations!")
+    } else console.log("Its a draw, press F5 to start again.")
+}
+
+console.log(game())
+console.log(score())
 
 
