ee7708314a30f2c0a2b9c444543bb39e83a4c2d1
Luciano <Luchoalexisbarboza@gmail.com>
2022-10-17 16:49:47 -0300

    Ahora funciona con botones!

--- Proyecto.js
+++ Proyecto.js
@@ -4,60 +4,48 @@ function getComputerChoice() {
     return random [Math.floor(Math.random()*random.length)];
 }
 
-function getPlayerChoice() {
-   return Player = prompt("Rock, Paper or Scissors?");
-}
-
-
-
-function playRound(playerSelection) {
-    let computerSelection = getComputerChoice();
-    if (playerSelection === "Rock".toLowerCase()) {
-        if (computerSelection === "Paper") {
-            computerScore++;
-            console.log("You lose");
-        } else if (computerSelection === "Scissors") {
-            playerScore++;
-            console.log("You win");
-        } else console.log("Draw");
-    } else if (playerSelection === "Paper".toLowerCase()) {
-        if (computerSelection === "Rock") {
-            playerScore++;
-            console.log("You win");
-        } else if (computerSelection === "Scissors") {
-            computerScore++;
-            console.log("You lose"); 
-        } else console.log("Draw");
-    } else if (playerSelection === "Scissors".toLowerCase()) {
-        if (computerSelection === "Rock") {
-            computerScore++;
-            console.log("You lose");
-        } else if (computerSelection === "Paper") {
-            playerScore++;
-            console.log("You win"); 
-        } else console.log("Draw");
-    }
-}
-
+const buttons = document.querySelectorAll('button');
+const player = document.getElementById('span')
+const computer = document.getElementById('span1')
 
 let computerScore = 0;
 let playerScore = 0;
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = getPlayerChoice();
-        playRound(playerSelection);            
-     }
-}
-
-function score() {
-    if (computerScore > playerScore) {
-        console.log("You lose this game, press F5 to start again.")
-    } else if (playerScore > computerScore) {
-        console.log("You win this game, Congratulations!")
-    } else console.log("Its a draw, press F5 to start again.")
-}
-console.log(game())
-console.log(score())
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    let computerSelection = getComputerChoice();
+    if (button.id === "1") {
+        if (computerSelection === "Paper") {
+            console.log("You lose");
+            computerScore++;
+            computer.textContent = computerScore;
+        } else if (computerSelection === "Scissors") {
+            console.log("You win");
+            playerScore++;
+            player.textContent = playerScore;
+        } else console.log("Draw");
+    } else if (button.id === "2") {
+        if (computerSelection === "Rock") {
+            console.log("You win");
+            playerScore++;
+            player.textContent = playerScore;
+        } else if (computerSelection === "Scissors") {
+            console.log("You lose"); 
+            computerScore++;
+            computer.textContent = computerScore;
+        } else console.log("Draw");
+    } else if (button.id === "3") {
+        if (computerSelection === "Rock") {
+            console.log("You lose"); 
+            computerScore++;
+            computer.textContent = computerScore;
+        } else if (computerSelection === "Paper") {
+            console.log("You win");
+            playerScore++;
+            player.textContent = playerScore;
+        } else console.log("Draw");
+    }
+  });
+});
 
 
